e0a6990a8e0e13212bd5808845d278764c9057a9
Amy Wightman <amy.wightman@quest.com>
2021-03-17 09:25:36 -0400

    added quiz page and starter js

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function score_quiz() {
+    console.log('score the quiz!');
+    var quiz = document.getElementById("quiz");
+    quiz.style.display = "none";
+    var results = document.getElementById("results");
+    results.style.display = "block";
+
+}
