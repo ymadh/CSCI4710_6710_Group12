06e757f9d28094a7bf446f3bbebe0bff9b56fad2
Connor Bullard <32420624+cdbullard@users.noreply.github.com>
2021-03-20 16:49:59 -0400

    Added second function for "skip" button

--- index.js
+++ index.js
@@ -8,31 +8,46 @@ function score_quiz() {
     results.style.display = "block";
 
     // default case
-    var icon = 'human.png';
-    if (document.getElementById('iconOptions1').checked) {
-        icon = 'vampire.jpg';
+    var icon = "human.png";
+    if (document.getElementById("iconOptions1").checked) {
+        icon = "vampire.jpg";
     }
-    if (document.getElementById('iconOptions2').checked) {
-        icon = 'human.png';
+    if (document.getElementById("iconOptions2").checked) {
+        icon = "human.png";
     }
 
-    if (document.getElementById('shadowRadio2').checked) {
+    if (document.getElementById("shadowRadio2").checked) {
         vampireScore += 4;
     };
 
-    if (document.getElementById('complexionRadio1').checked) {
+    if (document.getElementById("complexionRadio1").checked) {
         vampireScore += 3;
     }
 
-    if (document.getElementById('garlicRadio1').checked) {
+    if (document.getElementById("garlicRadio1").checked) {
         vampireScore += 3;
     }
 
     console.log(vampireScore);
     if (vampireScore > 6) {
-        document.getElementById('results').innerHTML = firstName + " " + lastName + " is a Vampire!"
+        document.getElementById("results").innerHTML = "is a Vampire!"
     } else {
-        document.getElementById('results').innerHTML = firstName + " " + lastName + " is not a Vampire!"
+        document.getElementById("results").innerHTML = "is not a Vampire!"
     }
 
+}
+
+function headsOrTails() {
+    var randomVampireScore = 0;
+    var quiz = document.getElementById("quiz");
+    quiz.style.display = "none";
+    var results = document.getElementById("results");
+    results.style.display = "block";
+
+    randomVampireScore = Math.floor(Math.random() * Math.floor(max))
+    if (vampireScore == 1) {
+        document.getElementById("results").innerHTML = "is a Vampire!"
+    } else {
+        document.getElementById("results").innerHTML = "is not a Vampire!"
+    }
 }
