bb3d0869c43f70c54194f9691f7047e30119b129
Amy Wightman <amy.wightman@quest.com>
2021-04-03 12:54:44 -0400

    created notes

--- hw5/TableCreateAndImportScript.sql
+++ hw5/TableCreateAndImportScript.sql
@@ -1,19 +1,20 @@
 --create table script
+
 CREATE TABLE public.hw5
 (
     index bigint,
-    "What country do you live in?" character varying(50) COLLATE pg_catalog."default",
-    "How old are you?" bigint,
-    "What is your gender?" character varying(50) COLLATE pg_catalog."default",
-    "To what extent do you feel FEAR due to the coronavirus?" bigint,
-    "To what extent do you feel ANXIOUS due to the coronavirus?" bigint,
-    "To what extent do you feel ANGRY due to the coronavirus?" bigint,
-    "To what extent do you feel HAPPY due to the coronavirus?" bigint,
-    "To what extent do you feel SAD due to the coronavirus?" bigint,
-    "Which emotion is having the biggest impact on you?" character varying(5000) COLLATE pg_catalog."default",
-    "What makes you feel that way?" character varying(5000) COLLATE pg_catalog."default",
-    "What brings you the most meaning during the coronavirus outbrea" character varying(5000) COLLATE pg_catalog."default",
-    "What is your occupation?" character varying(5000) COLLATE pg_catalog."default"
+    "country" character varying(50) COLLATE pg_catalog."default",
+    "age" bigint,
+    "gender" character varying(50) COLLATE pg_catalog."default",
+    "fear" bigint,
+    "anxious" bigint,
+    "anger" bigint,
+    "happy" bigint,
+    "sad" bigint,
+    "emotion" character varying(5000) COLLATE pg_catalog."default",
+    "desc" character varying(5000) COLLATE pg_catalog."default",
+    "meaning" character varying(5000) COLLATE pg_catalog."default",
+    "occupation" character varying(5000) COLLATE pg_catalog."default"
 )
 
 TABLESPACE pg_default;
@@ -23,4 +24,4 @@ ALTER TABLE public.hw5
 	
 	
 --Add in info (you would need to change the file path)
-copy hw5 FROM 'C:\Users\James\OneDrive\Desktop\Masters program\CSCI 6710\project5csv.csv' DELIMITER ',' CSV HEADER;
+copy hw5 FROM '/Users/adunphy/Sites/CSCI4710/CSCI4710-group12/hw5/data/project5csv.csv' DELIMITER ',' CSV HEADER;
