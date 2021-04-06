c53f0aa95963d1a4a91a3a65a29f7761bf741a96
Connor Bullard <32420624+cdbullard@users.noreply.github.com>
2021-04-05 21:20:57 -0400

    Added "Desc" column

--- hw5/main.py
+++ hw5/main.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/group12'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:pass@localhost/group12'
 db = SQLAlchemy(app)
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres@localhost:5432/group12"
 
@@ -24,10 +24,11 @@ class HwModel(db.Model):
     happy = db.Column(db.Integer())
     sad = db.Column(db.Integer())
     emotion = db.Column(db.String())
+    desc = db.Column(db.String())
     meaning = db.Column(db.String())
     occupation = db.Column(db.String())
 
-    def __init__(self, index, country, age, gender, fear, anxious, anger, happy, sad, emotion, meaning, occupation):
+    def __init__(self, index, country, age, gender, fear, anxious, anger, happy, sad, emotion, desc, meaning, occupation):
         self.index = index
         self.country = country
         self.age = age
@@ -38,6 +39,7 @@ class HwModel(db.Model):
         self.happy = happy
         self.sad = sad
         self.emotion = emotion
+        self.desc = description
         self.meaning = meaning
         self.occupation = occupation
 
@@ -53,7 +55,7 @@ def parse_data(query_result):
     result_list = []
     for element in query_result:
         result_list.append([element.index, element.country, element.age, element.gender, element.fear, element.anxious,
-                            element.anger, element.happy, element.sad, element.emotion, element.meaning, element.occupation])
+                            element.anger, element.happy, element.sad, element.emotion, element. desc, element.meaning, element.occupation])
     #print({'user_data': result_list})
     return result_list
 
