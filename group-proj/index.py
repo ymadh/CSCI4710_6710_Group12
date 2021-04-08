bdcd0e017b67ab8ddfbdfdfccc53c3ddbda2af06
Amy Wightman <amy.wightman@quest.com>
2021-04-07 20:06:46 -0400

    init group project files

---
+++ group-proj/index.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+
+@app.route("/history")
+def history():
+    return render_template('history.html')
+
+
+@app.route("/rent")
+def rent():
+    return render_template('rent.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
