0ce8beb2d6418019019dc34460505a029b07a330
Amy Wightman <amy.wightman@quest.com>
2021-04-11 16:54:03 -0400

    adding part 2 folder

---
+++ hw5-part2/main.py
@@ -0,0 +1,178 @@
+import os
+import json
+import util
+import psycopg2
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import and_
+
+app = Flask(__name__)
+
+connection = psycopg2.connect(user="postgres",
+                              password="",
+                              host="127.0.0.1",
+                              port="5432",
+                              database="group12")
+
+# all data
+cursor = connection.cursor()
+allDataQuer = "select * from hw5"
+cursor.execute(allDataQuer)
+allData = cursor.fetchall()
+
+# step 1
+cursor = connection.cursor()
+group1Query = "select * from hw5_group1"
+cursor.execute(group1Query)
+group1Data = cursor.fetchall()
+group2Query = "select * from hw5_group2"
+cursor.execute(group2Query)
+group2Data = cursor.fetchall()
+group3Query = "select * from hw5_group3"
+cursor.execute(group3Query)
+group3Data = cursor.fetchall()
+group4Query = "select * from hw5_group4"
+cursor.execute(group4Query)
+group4Data = cursor.fetchall()
+
+# steps 2 & 3
+cursor = connection.cursor()
+group1QueryA = "select * from hw5_group1_USA"
+cursor.execute(group1QueryA)
+group1DataA = cursor.fetchall()
+group1QueryB = "select * from hw5_group1_non_USA"
+cursor.execute(group1QueryB)
+group1DataB = cursor.fetchall()
+
+cursor = connection.cursor()
+group2QueryA = "select * from hw5_group2_USA"
+cursor.execute(group2QueryA)
+group2DataA = cursor.fetchall()
+group2QueryB = "select * from hw5_group2_non_USA"
+cursor.execute(group2QueryB)
+group2DataB = cursor.fetchall()
+
+cursor = connection.cursor()
+group3QueryA = "select * from hw5_group3_USA"
+cursor.execute(group3QueryA)
+group3DataA = cursor.fetchall()
+group3QueryB = "select * from hw5_group3_non_USA"
+cursor.execute(group3QueryB)
+group3DataB = cursor.fetchall()
+
+cursor = connection.cursor()
+group4QueryA = "select * from hw5_group4_USA"
+cursor.execute(group4QueryA)
+group4DataA = cursor.fetchall()
+group4QueryB = "select * from hw5_group4_non_USA"
+cursor.execute(group4QueryB)
+group4DataB = cursor.fetchall()
+
+column_names = ["index", "country", "age", "gender", "fear", "anxious", "angry",
+                "happy", "sad", "impact", "feeling", "meaning", "occupation"]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/alldata')
+def alldata():
+    labels = ['All Data']
+    return render_template('alldata.html',
+                           labels_html=labels,
+                           column_html=column_names,
+                           allData=allData
+                           )
+
+@app.route('/step1')
+def step1():
+    labels = ['Young Male', 'Older Male', 'Young Female', 'Older Female']
+    return render_template('step1.html',
+                           labels_html=labels,
+                           column_html=column_names,
+                           group1=group1Data,
+                           group2=group2Data,
+                           group3=group3Data,
+                           group4=group4Data
+                           )
+
+@app.route('/step2')
+def step2():
+    labels = ['Young Male USA',
+              'Young Male Non-USA',
+              'Older Male USA',
+              'Older Male Non-USA',
+              'Young Female USA',
+              'Young Female Non-USA',
+              'Older Female USA',
+              'Older Female Non-USA']
+    return render_template('step2.html',
+                           labels_html=labels,
+                           column_html=column_names,
+                           group1=group1DataA,
+                           group2=group1DataB,
+                           group3=group2DataA,
+                           group4=group2DataB,
+                           group5=group3DataA,
+                           group6=group3DataB,
+                           group7=group4DataA,
+                           group8=group4DataB
+                           )
+
+
+@app.route('/step3')
+def step3():
+    labels = [
+        'Young Male USA',
+        'Young Male Non-USA',
+        'Older Male USA',
+        'Older Male Non-USA',
+        'Young Female USA',
+        'Young Female Non-USA',
+        'Older Female USA',
+        'Older Female Non-USA']
+
+    data = []
+    if (len(group1DataA) > 10):
+        label_group = util.cluster_user_data(group1DataA)
+        data.append(util.split_user_data(group1DataA, label_group))
+
+    if (len(group1DataB) > 10):
+        label_group = util.cluster_user_data(group1DataB)
+        data.append(util.split_user_data(group1DataB, label_group))
+
+    if (len(group2DataA) > 10):
+        label_group = util.cluster_user_data(group2DataA)
+        data.append(util.split_user_data(group2DataA, label_group))
+
+    if (len(group2DataB) > 10):
+        label_group = util.cluster_user_data(group2DataB)
+        data.append(util.split_user_data(group2DataB, label_group))
+
+    if (len(group3DataA) > 10):
+        label_group = util.cluster_user_data(group3DataA)
+        data.append(util.split_user_data(group3DataA, label_group))
+
+    if (len(group3DataB) > 10):
+        label_group = util.cluster_user_data(group3DataB)
+        data.append(util.split_user_data(group3DataB, label_group))
+
+    if (len(group4DataA) > 10):
+        label_group = util.cluster_user_data(group4DataA)
+        data.append(util.split_user_data(group4DataA, label_group))
+
+    if (len(group4DataB) > 10):
+        label_group = util.cluster_user_data(group4DataB)
+        data.append(util.split_user_data(group4DataB, label_group))
+
+    return render_template('step3.html',
+                           labels_html=labels,
+                           column_html=column_names,
+                           data=data
+                           )
+
+if __name__ == '__main__':
+    app.debug = True
+    ip = '127.0.0.1'
+    app.run(host=ip)
---
+++ hw5-part2/util.py
@@ -0,0 +1,55 @@
+from sklearn.cluster import KMeans
+import numpy as np
+
+
+def cluster_user_data(input_data, emotional_col_start=4, emotional_col_end=9, n_clusters=3):
+    '''
+        This function cluster user data based on KMeans algorithm
+        By default, it will split your data into three groups
+        '''
+    # collect answers for five emotional questions
+    # which are located from 4th col to 9th col
+    emotional_data = [i[emotional_col_start:emotional_col_end]
+                      for i in input_data]
+    # use kmeans to cluster data
+    kmeans = KMeans(n_clusters).fit(emotional_data)
+    # return cluster labels
+    return kmeans.labels_
+
+
+def split_user_data(input_data, labels, n_clusters=3):
+    '''
+    this function will split input data into groups
+    based on labels
+    '''
+    result_list = []
+    for i in range(n_clusters):
+        # find indices of each group elements, without [0] the result is tuple
+        tmp_indices = np.where(labels == i)[0]
+        result_list.append([input_data[i] for i in tmp_indices])
+
+    return result_list
+    
+def split_data_for_hw(input_data):
+	
+	ym = []
+	om = []
+	yf = []
+	of = []
+	for data in input_data:
+		if data[3].lower() == 'male':
+			if data[2] <= 35:
+				ym.append(data)
+			else:
+				om.append(data)
+		else:
+			if data[2] <= 35:
+				yf.append(data)
+			else:
+				of.append(data)
+	result_list = []
+	result_list.append(ym)
+	result_list.append(om)
+	result_list.append(yf)
+	result_list.append(of)
+	return result_list
