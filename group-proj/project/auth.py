10831b59ace5a00705de2d23bf9fc18d5a716cf2
Amy Wightman <amy.wightman@quest.com>
2021-04-14 14:41:54 -0400

    added logout

--- group-proj/project/auth.py
+++ group-proj/project/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
-from flask_login import login_user
+from flask_login import current_user, login_user, login_required, logout_user
 from .models import User
 
 auth = Blueprint('auth', __name__)
@@ -53,5 +53,7 @@ def signup_post():
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('main.index'))
