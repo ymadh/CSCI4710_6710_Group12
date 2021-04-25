11c99296e5cca96f53cfcb13b44ef1151637c2e6
Connor Bullard <32420624+cdbullard@users.noreply.github.com>
2021-04-24 23:32:15 -0400

    Added messages block in login.html
    Trying to get the "flash" function to work in auth.py

--- group-proj/project/auth.py
+++ group-proj/project/auth.py
@@ -22,7 +22,7 @@ def login_post():
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again')
         # if the user doesn't exist or password is wrong, reload the page
-        time.sleep(2)
+        time.sleep(1)
         return redirect(url_for('main.login'))
 
     # if the above check passes, then we know the user has the right credentials
